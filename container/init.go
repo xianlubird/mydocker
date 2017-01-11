4b834f5f611c5a40f25dacdcc73b078fddfadbeb
bingshen.wbs <bingshen.wbs@alibaba-inc.com>
2017-01-11 17:55:15 +0800

    code-2.1

---
+++ container/init.go
@@ -0,0 +1,19 @@
+package container
+
+import (
+	"os"
+	"syscall"
+	"github.com/Sirupsen/logrus"
+)
+
+func RunContainerInitProcess(command string, args []string) error {
+	logrus.Infof("command %s", command)
+
+	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
+	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
+	argv := []string{command}
+	if err := syscall.Exec(command, argv, os.Environ()); err != nil {
+		logrus.Errorf(err.Error())
+	}
+	return nil
+}
