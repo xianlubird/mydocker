6ec5818a24974a2a91b42914a70065141fac7b80
yqszxx <maillist@yqszxx.org>
2020-03-12 23:24:42 +0800

    Change dependency manager to go mod.
    Signed-off-by: yqszxx <maillist@yqszxx.org>

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/xianlubird/mydocker
+
+go 1.14
+
+require (
+	github.com/Sirupsen/logrus v0.8.8-0.20151204141443-446d1c146faa
+	github.com/mholt/archiver v2.0.1-0.20161215173051-3db696109532+incompatible
+	github.com/nwaples/rardecode v0.0.0-20161222234428-18e2160743d3
+	github.com/ulikunitz/xz v0.5.2
+	github.com/urfave/cli v1.19.1
+	github.com/vishvananda/netlink v0.0.0-20170105235913-1890b34fa39d
+	github.com/vishvananda/netns v0.0.0-20161219181606-2c9454e4fc6e
+	golang.org/x/sys v0.0.0-20151211033651-833a04a10549
+)
--- network/bridge.go
+++ network/bridge.go
@@ -2,12 +2,12 @@ package network
 
 import (
 	"fmt"
+	log "github.com/Sirupsen/logrus"
+	"github.com/vishvananda/netlink"
 	"net"
+	"os/exec"
 	"strings"
 	"time"
-	"github.com/vishvananda/netlink"
-	log "github.com/Sirupsen/logrus"
-	"os/exec"
 )
 
 type BridgeNetworkDriver struct {
@@ -20,10 +20,10 @@ func (d *BridgeNetworkDriver) Name() string {
 func (d *BridgeNetworkDriver) Create(subnet string, name string) (*Network, error) {
 	ip, ipRange, _ := net.ParseCIDR(subnet)
 	ipRange.IP = ip
-	n := &Network {
-		Name: name,
+	n := &Network{
+		Name:    name,
 		IpRange: ipRange,
-		Driver: d.Name(),
+		Driver:  d.Name(),
 	}
 	err := d.initBridge(n)
 	if err != nil {
@@ -71,8 +71,6 @@ func (d *BridgeNetworkDriver) Connect(network *Network, endpoint *Endpoint) erro
 func (d *BridgeNetworkDriver) Disconnect(network Network, endpoint *Endpoint) error {
 	return nil
 }
-
-
 
 func (d *BridgeNetworkDriver) initBridge(n *Network) error {
 	// try to get bridge by name, if it already exists then just exit
@@ -148,7 +146,6 @@ func setInterfaceUP(interfaceName string) error {
 	return nil
 }
 
-
 // Set the IP addr of a netlink interface
 func setInterfaceIP(name string, rawIP string) error {
 	retries := 2
@@ -169,7 +166,7 @@ func setInterfaceIP(name string, rawIP string) error {
 	if err != nil {
 		return err
 	}
-	addr := &netlink.Addr{IPNet: ipNet, Peer: ipNet, Label: "", Flags: 0, Scope: 0, Broadcast: nil}
+	addr := &netlink.Addr{IPNet: ipNet, Peer: ipNet, Label: "", Flags: 0, Scope: 0}
 	return netlink.AddrAdd(iface, addr)
 }
 
